91e47cdec02d6db37e9ad44fb0820976f032e141
Paul Campbell <pcampbell@kemitix.net>
2018-04-13 20:08:59 +0100

    Remove sonarcube and add caddy

--- update.sh
+++ update.sh
@@ -4,10 +4,7 @@ cd `dirname $0`
 echo "Update to latest version of images..."
 (cd jenkins && ./build-jenkins.sh)
 docker-compose pull
-echo "Shutting down Jenkins Stack..."
-docker-compose down
 echo "Rebuilding and starting Jenkins Stack..."
 docker-compose up --build -d
 echo "Done."
-docker-compose logs -f
 
